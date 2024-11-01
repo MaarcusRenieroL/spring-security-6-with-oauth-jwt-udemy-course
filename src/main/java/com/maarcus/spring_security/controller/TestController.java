81757aa3dc5444fd39916b2037fb34a8dccf646d
MaarcusRenieroL <maarcusreniero.l@gmail.com>
2024-11-01 15:13:10 +0530

    feat: add test controller

---
+++ src/main/java/com/maarcus/spring_security/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.maarcus.spring_security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+  @GetMapping("/hello")
+  public String hello() {
+    return "Hello";
+  }
+
+  @GetMapping("/hi")
+  public String hi() {
+      return "Hi";
+  }
+  
+}
