7c7c66d5d6581c78afdad341f201590d4309aa24
MaarcusRenieroL <maarcusreniero.l@gmail.com>
2024-11-01 15:08:12 +0530

    feat: setup spring project and security

---
+++ src/test/java/com/maarcus/spring_security/SpringSecurityApplicationTests.java
@@ -0,0 +1,11 @@
+package com.maarcus.spring_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
