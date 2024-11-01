b321c946694e930a7ec9562684fd9f26527e93fa
MaarcusRenieroL <maarcusreniero.l@gmail.com>
2024-11-01 19:59:02 +0530

    feat: add code to add in memory authentication

--- src/main/java/com/maarcus/spring_security/config/SecurityConfig.java
+++ src/main/java/com/maarcus/spring_security/config/SecurityConfig.java
@@ -6,21 +6,50 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
   @Bean
   SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-    http.authorizeHttpRequests((requests) -> requests.requestMatchers("/contact").permitAll());
-    http.authorizeHttpRequests((requests) -> requests.requestMatchers("/hi").denyAll());
-    http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+    http.authorizeHttpRequests(
+        (requests) ->
+            requests
+                .requestMatchers("/contact")
+                .permitAll()
+                .requestMatchers("/hi")
+                .denyAll()
+                .anyRequest()
+                .authenticated());
     // http.formLogin(withDefaults());
+    http.csrf(AbstractHttpConfigurer::disable);
     http.sessionManagement(
         session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
     http.httpBasic(withDefaults());
     return http.build();
+  }
+
+  @Bean
+  UserDetailsService userDetailsService() {
+    InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+
+    if (!manager.userExists("user1")) {
+      manager.createUser(
+          User.withUsername("user1").password("{noop}password1").roles("USER").build());
+    }
+
+    if (!manager.userExists("admin")) {
+      manager.createUser(
+          User.withUsername("admin").password("{noop}adminPassword").roles("ADMIN").build());
+    }
+
+    return manager;
   }
 }
