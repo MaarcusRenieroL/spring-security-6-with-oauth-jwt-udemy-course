656ee7d5aff731531d5bc8a26fc00648d5c1729b
MaarcusRenieroL <maarcusreniero.l@gmail.com>
2024-11-02 11:03:44 +0530

    feat: add basic authentication by fetching users from database

--- src/main/java/com/maarcus/spring_security/config/SecurityConfig.java
+++ src/main/java/com/maarcus/spring_security/config/SecurityConfig.java
@@ -2,17 +2,19 @@ package com.maarcus.spring_security.config;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
-import javax.sql.DataSource;
+import com.maarcus.spring_security.model.AppRole;
+import com.maarcus.spring_security.model.Role;
+import com.maarcus.spring_security.model.User;
+import com.maarcus.spring_security.repository.RoleRepository;
+import com.maarcus.spring_security.repository.UserRepository;
+import java.time.LocalDate;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-// import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -39,20 +41,43 @@ public class SecurityConfig {
   }
 
   @Bean
-  UserDetailsService userDetailsService(DataSource dataSource) {
-    // InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager()
-    JdbcUserDetailsManager manager = new JdbcUserDetailsManager(dataSource);
+  public CommandLineRunner initData(RoleRepository roleRepository, UserRepository userRepository) {
+    return args -> {
+      Role userRole =
+          roleRepository
+              .findByRoleName(AppRole.USER)
+              .orElseGet(() -> roleRepository.save(new Role(AppRole.USER)));
+      Role adminRole =
+          roleRepository
+              .findByRoleName(AppRole.ADMIN)
+              .orElseGet(() -> roleRepository.save(new Role(AppRole.ADMIN)));
 
-    if (!manager.userExists("user1")) {
-      manager.createUser(
-          User.withUsername("user1").password("{noop}password1").roles("USER").build());
-    }
-
-    if (!manager.userExists("admin")) {
-      manager.createUser(
-          User.withUsername("admin").password("{noop}adminPassword").roles("ADMIN").build());
-    }
-
-    return manager;
+      if (userRepository.findByUserName("user1").isEmpty()) {
+        User user1 = new User("user1", "{noop}password1", "user1@example.com");
+        user1.setAccountNonLocked(false);
+        user1.setAccountNonExpired(true);
+        user1.setCredentialsNonExpired(true);
+        user1.setEnabled(true);
+        user1.setCredentialsExpiryDate(LocalDate.now().plusYears(1));
+        user1.setAccountExpiryDate(LocalDate.now().plusYears(1));
+        user1.setTwoFactorEnabled(false);
+        user1.setSignUpMethod("email");
+        user1.setRole(userRole);
+        userRepository.save(user1);
+      }
+      if (userRepository.findByUserName("admin").isEmpty()) {
+        User admin = new User("admin", "{noop}adminPass", "admin@example.com");
+        admin.setAccountNonLocked(true);
+        admin.setAccountNonExpired(true);
+        admin.setCredentialsNonExpired(true);
+        admin.setEnabled(true);
+        admin.setCredentialsExpiryDate(LocalDate.now().plusYears(1));
+        admin.setAccountExpiryDate(LocalDate.now().plusYears(1));
+        admin.setTwoFactorEnabled(false);
+        admin.setSignUpMethod("email");
+        admin.setRole(adminRole);
+        userRepository.save(admin);
+      }
+    };
   }
 }
