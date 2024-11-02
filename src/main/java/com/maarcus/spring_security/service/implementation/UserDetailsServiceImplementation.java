8ab5c81e90a64d6017f44218a1b28c0862224cc0
MaarcusRenieroL <maarcusreniero.l@gmail.com>
2024-11-02 10:46:35 +0530

    feat: add custom implementation of user details and user details service interface

---
+++ src/main/java/com/maarcus/spring_security/service/implementation/UserDetailsServiceImplementation.java
@@ -0,0 +1,29 @@
+package com.maarcus.spring_security.service.implementation;
+
+import com.maarcus.spring_security.model.User;
+import com.maarcus.spring_security.repository.UserRepository;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImplementation implements UserDetailsService {
+
+  @Autowired private UserRepository userRepository;
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    // TODO Auto-generated method stub
+
+    Optional<User> optionalUser = userRepository.findByUserName(username);
+
+    if (optionalUser.isPresent()) {
+      return UserDetailsImplementation.build(optionalUser.get());
+    }
+
+    throw new UsernameNotFoundException("User not found with the username " + username);
+  }
+}
