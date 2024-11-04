0110399b213e8519a31e1bffa5fb6c2cb5db2881
MaarcusRenieroL <maarcusreniero.l@gmail.com>
2024-11-04 15:06:56 +0530

    feat: add api for sign in

--- src/main/java/com/maarcus/spring_security/config/SecurityConfig.java
+++ src/main/java/com/maarcus/spring_security/config/SecurityConfig.java
@@ -13,6 +13,8 @@ import java.time.LocalDate;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -27,6 +29,12 @@ import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 @EnableWebSecurity
 @EnableMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfig {
+
+  @Bean
+  AuthenticationManager authenticationManager(
+      AuthenticationConfiguration authenticationConfiguration) throws Exception {
+    return authenticationConfiguration.getAuthenticationManager();
+  }
 
   @Bean
   SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
--- src/main/java/com/maarcus/spring_security/controller/AuthController.java
+++ src/main/java/com/maarcus/spring_security/controller/AuthController.java
@@ -1,22 +1,70 @@
 package com.maarcus.spring_security.controller;
 
+import com.maarcus.spring_security.payload.request.LoginRequest;
+import com.maarcus.spring_security.payload.response.LoginResponse;
+import com.maarcus.spring_security.utils.JwtUtils;
 import jakarta.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/auth/")
+@RequestMapping("/api/auth")
 public class AuthController {
+
+  @Autowired private JwtUtils jwtUtils;
+  @Autowired AuthenticationManager authenticationManager;
 
   @GetMapping("csrf-token")
   public CsrfToken getCsrfToken(HttpServletRequest request) {
     return (CsrfToken) request.getAttribute(CsrfToken.class.getName());
   }
 
-  @GetMapping
-  public String signIn() {
-    return "Authenticated Successfully";
+  @PostMapping("/sign-in")
+  public ResponseEntity<?> authenticateUser(@RequestBody LoginRequest loginRequest) {
+    Authentication authentication;
+    try {
+      authentication =
+          authenticationManager.authenticate(
+              new UsernamePasswordAuthenticationToken(
+                  loginRequest.getUsername(), loginRequest.getPassword()));
+    } catch (AuthenticationException exception) {
+
+      Map<String, Object> map = new HashMap<>();
+
+      map.put("Message", "Bad credentials");
+      map.put("Status", false);
+
+      return new ResponseEntity<Object>(map, HttpStatus.NOT_FOUND);
+    }
+
+    SecurityContextHolder.getContext().setAuthentication(authentication);
+
+    UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+
+    String jwtToken = jwtUtils.generateTokenFromUsername(userDetails);
+    List<String> roles =
+        userDetails.getAuthorities().stream()
+            .map(item -> item.getAuthority())
+            .collect(Collectors.toList());
+
+    LoginResponse response = new LoginResponse(userDetails.getUsername(), jwtToken, roles);
+    return ResponseEntity.ok(response);
   }
 }
